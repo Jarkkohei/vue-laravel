ba9203fc93084143e7e8aef1f1c521fd17dae13b
Jarkko Heikkinen <jarkko.heikkinen@mediasignal.fi>
2018-04-11 14:27:28 +0300

    Added the view and functionality for creating new quotes. #VUE.JS 2 FRONTEND 1/3

---
+++ src/components/new-quote.vue
@@ -0,0 +1,30 @@
+<template>
+    <form @submit.prevent="onSubmitted">
+        <div class="form-group">
+            <label for="content">Content</label>
+            <input type="text" id="content" class="form-control" v-model="quoteContent">
+        </div>
+        <button class="btn btn-primary" type="submit">Submit</button>
+    </form>
+</template>
+
+<script>
+import axios from 'axios';
+    export default {
+        data() {
+            return {
+                quoteContent: ''
+            }
+        },
+        methods: {
+            onSubmitted() {
+                axios.post('http://localhost:8000/api/quote', { content: this.quoteContent })
+                    .then((response) => console.log(response)
+                )
+                .catch(
+                    (error) => console.log(error)
+                );
+            }
+        }
+    }
+</script>
